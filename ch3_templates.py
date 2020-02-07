d028c3043b4231308712cc075d7ffe9ab1039847
Onurhan <onurhan.hamzaoglu@gmail.com>
2020-02-07 12:19:12 +0300

    Using variables and variable filters for Jinja2 templates

--- ch3_templates.py
+++ ch3_templates.py
@@ -12,6 +12,15 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user_info.html',  name=name)
+
+# VARIABLES - Jinja2 -> Template
+users = {'Onurhan': 27, 'Dark Knight': 35}
+movie = 'The Lord of The Rings'
+snacks = ['popcorn', 'bagels', 'cookie']
+
+@app.route('/variables')
+def variables():
+    return render_template('variables.html', users=users, movie=movie, snacks=snacks)
     
 
 if __name__ == "__main__":
